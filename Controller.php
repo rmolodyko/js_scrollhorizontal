aa4ee1b64aa471a0f226455a619b5f702bddde4c
Molodyko Ruslan <molodyko@samsonos.com>
2015-06-30 15:50:04 +0300

    Init

---
+++ Controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace samson\js\scrollhorizontal;
+
+/**
+ * Интерфейс для подключения модуля в ядро фреймворка SamsonPHP
+ * @author Ruslan Molodyko <molodyko@samsonos.com>
+ */
+class Controller extends \samson\core\CompressableExternalModule
+{	
+	/** Identifier */
+	protected $id = 'scrollhorizontal';
+}
